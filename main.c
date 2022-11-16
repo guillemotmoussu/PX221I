53b975434807a65a8ec7d3c06490c0dcf8d95c54
Guillemot Moussu <guillemot.moussu@gmail.com>
2022-11-16 10:12:59 +0100

    Mi-séance

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#include <assert.h>
 
 #define EmptyRank {'-','-','-','-','-','-','-','-'}
 #define StartBoard {EmptyRank,EmptyRank,EmptyRank,EmptyRank,EmptyRank,EmptyRank,EmptyRank,EmptyRank}
+#define J1 'X'
+#define J2 'O'
 
 struct Game
 {
@@ -35,8 +36,7 @@ char MoveLegal(struct Game Game, int dx, int dy)
     {
         if (Game.Board[Game.y+i*dy][Game.x+i*dx]=='-') return 1;
         if (Game.Board[Game.y+i*dy][Game.x+i*dx]==Game.Who) return i==1;
-        if (Game.Board[Game.y+i*dy][Game.x+i*dx]==167-Game.Who) i++;
-        else assert(!printf("MoveLegal failed"));
+        if (Game.Board[Game.y+i*dy][Game.x+i*dx]==J1+J2-Game.Who) i++;
     }
     return 1;
 }
@@ -76,7 +76,7 @@ void AskMove(struct Game *Game)
     do
     {
         printf("\nPlayer %c, place a disk, using A1 - H8 format: ",Game->Who);
-        scanf("%2s",coords);   for(char buf=0;buf!='\n';scanf("%c",&buf)); // buffer destroyed
+        scanf("%2s",coords);   for(char buf=0;buf!='\n';scanf("%c",&buf)); // buffer destroyed 
         Game->x=coords[0]-'A'; // 'A' = 65
         Game->y=coords[1]-'1'; // '1' = 49
     } while (MoveValid(*Game));
@@ -89,13 +89,12 @@ void FlipMove(struct Game *Game, int dx, int dy)
     while((Game->x+i<7 || dx!=1) && (Game->y+i<7 || dy!=1) && (Game->x>i || dx!=-1) && (Game->y>i || dy!=-1))
     {
         if (Game->Board[Game->y+i*dy][Game->x+i*dx]==Game->Who) return;
-        if (Game->Board[Game->y+i*dy][Game->x+i*dx]==167-Game->Who)
+        if (Game->Board[Game->y+i*dy][Game->x+i*dx]==J1+J2-Game->Who)
         {
             Game->Board[Game->y+i*dy][Game->x+i*dx]=Game->Who;
             i++;
         }
     }
-    assert(!printf("FlipMove failed"));
     return;
 }
 
@@ -113,34 +112,53 @@ void ExeMove(struct Game *Game)
     return;
 }
 
-char GameOver(struct Game Game)
+char GameNotOver(struct Game Game)
 {
     for(char y=0;y<8;y++)
     {
         for(char x=0;x<8;x++)
         {
-            if (Game.Board[y+0][x+0]=='-') return 1;
+            Game.x=x;
+            Game.y=y;
+            if (MoveLegalAll(Game)) return 1;
+            Game.Who=J1+J2-Game.Who;
+            if (MoveLegalAll(Game)) return 1;
         }
     }
-    // Unfinished yet, only check if board is full
     return 0;
+}
+
+void game_ended(struct Game Game)
+{
+    int O_disks=0,X_disks=0;
+    for(int y=0;y<8;y++)
+    {
+        for(int x=0;x<8;x++)
+        {
+            if (Game.Board[y][x]==J1) X_disks++;
+            if (Game.Board[y][x]==J2) O_disks++;
+        }
+    }
+    printf("\nLa partie est terminée, avec %i pour les O et %i pour les X",O_disks,X_disks);
+    return;
 }
 
 int main()
 {
     printf("\nWelcome to my playable version of Reversi !\n");
-    struct Game Game={StartBoard,0,0,'X'};
-    Game.Board[3][3]='O';
-    Game.Board[3][4]='X'; // 'O' = 79
-    Game.Board[4][3]='X'; // 'X' = 88
-    Game.Board[4][4]='O';
-    while (GameOver(Game))
+    struct Game Game={StartBoard,0,0,J1};
+    Game.Board[3][3]=J2;
+    Game.Board[3][4]=J1;
+    Game.Board[4][3]=J1;
+    Game.Board[4][4]=J2;
+    while (GameNotOver(Game))
     {
         PrintBoard(Game);
         AskMove(&Game);
         ExeMove(&Game);
-        Game.Who=167-Game.Who; // 79+88=167
+        Game.Who=J1+J2-Game.Who;
     }
+
     // Add conclusion
     return 0;
 }
